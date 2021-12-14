5d58f30bf2081881f61e3c07432bc7aac8beeed0
Gokanb <gokan.bektas90@gmail.com>
2021-12-14 15:04:35 -0500

    .

---
+++ Digital Forensics/forensic recipies/multi_process.py
@@ -0,0 +1,42 @@
+#!/bin/usr/env python3
+
+import logging
+import multiprocessing as mp
+from random import randint
+import sys
+import time
+
+'''
+Description:  
+        -->This script will 
+To run this script--> python multi_process.py
+example --> 
+'''
+__authors__ = ["Gokan Bektas"]
+__date__ = 20211214
+__version__ = 1.0   # version can be always change so make sure you update after make a change. it has to be in quation. 
+__description__ = ' '
+
+def sleeping(seconds):
+    proc_name = mp.current_process().name
+    logger.info(f'{proc_name} is sleeping for {seconds} seconds.')
+    time.sleep(seconds)
+    
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+msg_fmt = logging.Formatter("%(asctime)-15s %(funcName)-7s" "%(levelname)-8s %(message)s")
+str_hndl = logging.StreamHandler(sys.stdout)
+str_hndl.setFormatter(fmt=msg_fmt)
+logger.addHandler(str_hndl)
+
+num_workers = 10
+workers = []
+for i in range(num_workers):
+    proc = mp.Process(target=sleeping, args=(randint(1, 20),))
+    proc.start()
+    workers.append(proc)
+    
+for worker in workers:
+    worker.join()
+    logger.info("Joined process {}".format(worker.name))
+    
